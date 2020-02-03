9aab8296aad6745dd0c90df599112716d3b3956e
G4br13l4 <gabolf93@gmail.com>
2020-02-03 11:49:41 -0600

    Add Javascript animation to Intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,27 +1,51 @@
 import React from 'react';
 
+let counter = 0;
+const skills = [
+    'Front-End Development', 
+    'Web Analytics',
+    'Digital Marketing',
+    'Prototypes'
+];
+
 class Intro extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            openHamburger: false
+            skill: skills[0],
+            showText: true
         };
     }
+
+    componentDidMount() {
+        setInterval(this.change, 2500);
+    }
   
-    toggleHamburger = () => {
-        let toggle = this.state.openHamburger;
-        this.setState({openHamburger: !toggle});
+    change = () => {
+        this.setState({showText: false});
+
+        setTimeout(() => {
+            this.setState({skill: skills[counter]});
+            this.setState({showText: true});
+            counter++;
+            if (counter >= skills.length) {
+                counter = 0;
+            }
+        }, 500);   
     }
 
     render() {
+        const { showText, skill } = this.state;
         return (
-            <header id="intro" class="intro">
-                <div class="intro__boxtxt">
-                    <h1 class="intro__text">
+            <header id="intro" className="intro">
+                <div className="intro__boxtxt">
+                    <h1 className="intro__text">
                         <span>Hola, mi nombre es </span>
-                        <strong class="name">Gabriela López Fuentes</strong>
+                        <strong className="name">Gabriela López Fuentes</strong>
                         <span>Me dedico a</span>
-                        <span class="anim-text"></span>
+                        <span className={`anim-text ${showText ? 'show' : 'hide'}`}>
+                            {skill}
+                        </span>
                     </h1>
                 </div>
             </header>
