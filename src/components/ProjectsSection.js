7e14417b7dddc1f40d5e543e53a92781a5460df2
G4br13l4 <gabolf93@gmail.com>
2020-02-04 00:56:51 -0600

    Add links to projects

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -20,36 +20,36 @@ class ProjectsSection extends React.Component {
                         name='Pokedex'
                         idCSS='pokedex'
                         tools='CSS3, Bootstrap 3, Javascript, jQuery, PokeApi API.'
-                        linkCode='https://open.spotify.com/collection/tracks'
-                        linkDemo='https://open.spotify.com/collection/tracks'
+                        linkCode='https://github.com/G4br13l4/pokedex'
+                        linkDemo='https://g4br13l4.github.io/pokedex/'
                     />
                     <Project
                         name='The Giphy Box'
                         idCSS='giphybox'
                         tools='CSS3, Bootstrap 3, Javascript, React, Giphy API.'
-                        linkCode='https://open.spotify.com/collection/tracks'
-                        linkDemo='https://open.spotify.com/collection/tracks'
+                        linkCode='https://github.com/G4br13l4/giphbox'
+                        linkDemo='https://g4br13l4.github.io/giphbox/'
                     />
                     <Project
                         name='Puzzle Game'
                         idCSS='puzzlegame'
                         tools='CSS3, Javascript, React.'
-                        linkCode='https://open.spotify.com/collection/tracks'
-                        linkDemo='https://open.spotify.com/collection/tracks'
+                        linkCode='https://github.com/G4br13l4/15puzzlegame'
+                        linkDemo='https://g4br13l4.github.io/15puzzlegame/'
                     />
                     <Project
                         name='Star Wars Cheat-sheet'
                         idCSS='starwars'
                         tools='CSS3, Javascript, React, Swapi API.'
-                        linkCode='https://open.spotify.com/collection/tracks'
-                        linkDemo='https://open.spotify.com/collection/tracks'
+                        linkCode='https://github.com/G4br13l4/star-wars-cheat-sheet'
+                        linkDemo='https://g4br13l4.github.io/star-wars-cheat-sheet/'
                     />
                     <Project
                         name='Silvia Peligro'
                         idCSS='speligro'
                         tools='Prototipo hecho con Figma e InVision. Actualmente en etapa de desarrollo con React.'
-                        linkCode='https://open.spotify.com/collection/tracks'
-                        linkDemo='https://open.spotify.com/collection/tracks'
+                        linkCode='#'
+                        linkDemo='#'
                     />
                 </div>
                 </section>
