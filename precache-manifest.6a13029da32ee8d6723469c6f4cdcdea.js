0d5722323426c4410aa147bf5bc55899ff7dcc48
G4br13l4 <gabolf93@gmail.com>
2020-02-04 18:35:53 -0600

    Updates

---
+++ precache-manifest.6a13029da32ee8d6723469c6f4cdcdea.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f14cea2d9410edd7c3e582ff13d6562",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "c380c47ffc10cb2a1095",
+    "url": "/my-portfolio/static/css/main.40472e04.chunk.css"
+  },
+  {
+    "revision": "5bbcf68a8aaf15196487",
+    "url": "/my-portfolio/static/js/2.c887c15f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-portfolio/static/js/2.c887c15f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c380c47ffc10cb2a1095",
+    "url": "/my-portfolio/static/js/main.dd6e9466.chunk.js"
+  },
+  {
+    "revision": "164cd71dc8e56a933ce3",
+    "url": "/my-portfolio/static/js/runtime-main.d96a2d46.js"
+  },
+  {
+    "revision": "cbd4e701269338259ee0b39a0b768167",
+    "url": "/my-portfolio/static/media/RobotoCondensed-Bold.cbd4e701.ttf"
+  },
+  {
+    "revision": "885e5558c90864ddb72ad486f36470cc",
+    "url": "/my-portfolio/static/media/RobotoCondensed-Light.885e5558.ttf"
+  },
+  {
+    "revision": "52ee8b598488b1ffbaa93e50cbd6a2f4",
+    "url": "/my-portfolio/static/media/RobotoCondensed-Regular.52ee8b59.ttf"
+  },
+  {
+    "revision": "4ef70659e6a13e94ac9c02a3715b18bd",
+    "url": "/my-portfolio/static/media/giphybox.4ef70659.jpg"
+  },
+  {
+    "revision": "60b7aa3eebdf68a6d6beef50e2022b52",
+    "url": "/my-portfolio/static/media/pokemon.60b7aa3e.jpg"
+  },
+  {
+    "revision": "946f299b05f633bdc839844b37d6d8ce",
+    "url": "/my-portfolio/static/media/puzzlegame.946f299b.jpg"
+  },
+  {
+    "revision": "bc5324fa49f97ff49bab240e8a91b690",
+    "url": "/my-portfolio/static/media/space.bc5324fa.jpg"
+  },
+  {
+    "revision": "b684409100d6d8f2a6a932fc914ab01b",
+    "url": "/my-portfolio/static/media/speligro.b6844091.jpg"
+  },
+  {
+    "revision": "03095e3a3fc935ee5d1f55185e475452",
+    "url": "/my-portfolio/static/media/starwars.03095e3a.jpg"
+  }
+]);
